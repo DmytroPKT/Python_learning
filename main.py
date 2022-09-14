29dd97cb3dcb5c4ffb310edc0bdbc6e177929a5f
DmytroPKT <demetriusz.pk@gmail.com>
2022-09-14 11:50:25 +0300

    1st Lesson

---
+++ main.py
@@ -0,0 +1,83 @@
+print('MATH OPERATORS:')
+print('''1.Operator: **\n\
+Operation: Exponent \n\
+Example: 2**3\n\
+Evaluates to: 8''')
+print(2**3)
+print('~' * 30)
+
+print('''2.Operator: %\n\
+Operation: Modulus/remainder \n\
+Example: 22%8\n\
+Evaluates to: 6''')
+print(22 % 8)
+print('~' * 30)
+
+print('''3.Operator: //\n\
+Operation: Integer division/floored quotient \n\
+Example: 67//6\n\
+Evaluates to: 11''')
+print(67 // 6)
+print('~' * 30)
+
+print('''4.Operator: /\n\
+Operation: Division \n\
+Example: 67/6\n\
+Evaluates to: 11.166666666666666''')
+print(67 / 6)
+print('~' * 30)
+
+print('''5.Operator: *\n\
+Operation: Multiplication \n\
+Example: 4*2\n\
+Evaluates to: 8''')
+print(4 * 2)
+print('~' * 30)
+
+print('''6.Operator: -\n\
+Operation: Subtraction \n\
+Example: 10 - 5\n\
+Evaluates to: 5''')
+print(10 - 5)
+print('~' * 30)
+
+print('''7.Operator: +\n\
+Operation: Addition \n\
+Example: 7 + 2\n\
+Evaluates to: 9''')
+print(7 + 2)
+print('~' * 30)
+print('~' * 30)
+print('~' * 30)
+print('Data types')
+print('''1.Integers: -2, -5, 0, 1, 2, 3\n\
+2.Floating-point numbers: -1.25, -1.0, -0.5, 0.0, 0.5, 1.0, 1.23\n\
+3.Strings: "a", "aa", "Hello" ''')
+print('~' * 30)
+
+print('''String Concatenation\n\
+Example: "Alice" + "Bob"\n\
+Evaluates to: AliceBob''')
+print('Alice' + 'Bob')
+print('~' * 30)
+
+print('''String Replication\n\
+Example: "Alice" * 5 \n\
+Evaluates to: AliceAliceAliceAliceAlice''')
+print('Alice' * 5)
+print('~' * 30)
+
+_45 = 35
+_948 = _45 * 10
+print(_948)
+print('~' * 30)
+
+print('Hello!')
+print('What is your name?')
+myName = input()
+print('It is nice to meet you, ' + myName)
+print('The length of your name is: ')
+print(len(myName))
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year')
