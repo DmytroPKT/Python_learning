256c088fb8a45d20e8ab212cf57252363bf4a533
DmytroPKT <demetriusz.pk@gmail.com>
2022-09-14 17:55:24 +0300

    introduction

---
+++ page2.py
@@ -0,0 +1,14 @@
+name = input('Enter your name: ')
+if name == 'Name':
+    print('Hi,' + name)
+if name == 'Dresden':
+    print('Fuck off ' + name)
+else:
+    print('Who are fucking you ' + name + '?')
+
+while True:
+    print('Fuck off', name)
+    name = input()
+    if name == 'Fuck you too':
+        break
+print('Okay')
