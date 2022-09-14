74fc565751bfe221e351e437cf910564ca2db36e
DmytroPKT <demetriusz.pk@gmail.com>
2022-09-14 15:24:08 +0300

    Page 1 completed

--- main.py
+++ main.py
@@ -5,42 +5,42 @@ Example: 2**3\n\
 Evaluates to: 8''')
 print(2**3)
 print('~' * 30)
-
+#
 print('''2.Operator: %\n\
 Operation: Modulus/remainder \n\
 Example: 22%8\n\
 Evaluates to: 6''')
 print(22 % 8)
 print('~' * 30)
-
+#
 print('''3.Operator: //\n\
 Operation: Integer division/floored quotient \n\
 Example: 67//6\n\
 Evaluates to: 11''')
 print(67 // 6)
 print('~' * 30)
-
+#
 print('''4.Operator: /\n\
 Operation: Division \n\
 Example: 67/6\n\
 Evaluates to: 11.166666666666666''')
 print(67 / 6)
 print('~' * 30)
-
+#
 print('''5.Operator: *\n\
 Operation: Multiplication \n\
 Example: 4*2\n\
 Evaluates to: 8''')
 print(4 * 2)
 print('~' * 30)
-
+#
 print('''6.Operator: -\n\
 Operation: Subtraction \n\
 Example: 10 - 5\n\
 Evaluates to: 5''')
 print(10 - 5)
 print('~' * 30)
-
+#
 print('''7.Operator: +\n\
 Operation: Addition \n\
 Example: 7 + 2\n\
@@ -54,24 +54,24 @@ print('''1.Integers: -2, -5, 0, 1, 2, 3\n\
 2.Floating-point numbers: -1.25, -1.0, -0.5, 0.0, 0.5, 1.0, 1.23\n\
 3.Strings: "a", "aa", "Hello" ''')
 print('~' * 30)
-
+#
 print('''String Concatenation\n\
 Example: "Alice" + "Bob"\n\
 Evaluates to: AliceBob''')
 print('Alice' + 'Bob')
 print('~' * 30)
-
+#
 print('''String Replication\n\
 Example: "Alice" * 5 \n\
 Evaluates to: AliceAliceAliceAliceAlice''')
 print('Alice' * 5)
 print('~' * 30)
-
+#
 _45 = 35
 _948 = _45 * 10
 print(_948)
 print('~' * 30)
-
+#
 print('Hello!')
 print('What is your name?')
 myName = input()
@@ -81,3 +81,17 @@ print(len(myName))
 print('What is your age?')
 myAge = input()
 print('You will be ' + str(int(myAge) + 1) + ' in a year')
+print('~' * 30)
+#
+print("""The line print('Hello, world!') means\n\
+ “Print out the text in the string 'Hello, world!'.\n\
+ ” When Python executes this line, you say that Python\n\
+ is calling the print() function and the string value\n\
+ is being passed to the function. A value that is passed\n\
+ to a function call is an argument. Notice that the quotes\n\
+ are not printed to the screen. They just mark where the string begins and ends;\n\
+ they are not part of the string value. print('LALALA') LALALA - argument""")
+print('~' * 30)
+#
+str(25)
+print('WTF is going on? This car is already ' + str(25) + ' years old.')
